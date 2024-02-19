d01b922d5a3f8a15889ab4422c3df32db56a628f
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-19 20:38:54 +0000

    py

---
+++ 01_basics/16_string_function.py
@@ -0,0 +1,10 @@
+'''
+
+string function: 
+'''
+str = " I am a coder."
+print(str.endswith("er."))#return true if string ends with substr
+print(str.capitalize())#capitalizes 1st char
+print(str.replace("a","y"))#replaces all occurrence of old with new
+print(str.find("o")) #returns 1 st index of 1st occurence
+print(str.count("am"))#counts the occurence of substr in string
