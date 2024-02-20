478a87f737cb865ae3702ce9126120a954986d9f
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-20 12:38:45 +0000

    py

---
+++ 02_basics/03_dictionaries.py
@@ -0,0 +1,10 @@
+#dictionaries are used to store data values in key:value pairs
+'''
+They are unordered , mutable (changeable)& dont allow duplicate keys
+'''
+dic={
+    "name":"Megha",
+    "cgpa":9.21,
+    "marks":[98,97,95],
+}
+print(dic)
