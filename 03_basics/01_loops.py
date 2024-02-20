76e506a5b638a1c3ebd77c7bea2f871d8e5aea28
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-20 17:53:11 +0000

    py

---
+++ 03_basics/01_loops.py
@@ -0,0 +1,18 @@
+'''
+Loops are used to repeat instructions.
+while Loops
+
+while condition:
+    #some work
+
+    '''
+
+# while True:
+#     print("Hello") #infinite loop
+    
+count = 1 
+while count <= 5:
+    print(count,"Hello Megha")
+    
+    count += 1
+print(count)
