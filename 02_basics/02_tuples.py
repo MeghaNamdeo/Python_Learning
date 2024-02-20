e9e281685787632627f3d741ab52ad313c8f234e
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-20 10:44:06 +0000

    py

--- 02_basics/02_tuples.py
+++ 02_basics/02_tuples.py
@@ -41,3 +41,45 @@ print(tup7.index(3)) #  output : 2, return index of first occurence of 3
 print(tup7.count(1)) # output : 3,counts total occurence  of 1 
 print(tup7)
  
+
+ #practice question 
+''' WAP to ask the user to enter names of their 3 favourite movies & store them in a list '''
+
+movies=[]
+movie1 = input("Enter 1st favourite movie : ")
+movie2 = input("Enter 2nt favourite movie : ")
+movie3 = input("Enter 3rd favourite movie : ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
+
+'''
+output : 
+Enter 1st favourite movie: Inception
+Enter 2nd favourite movie: The Shawshank Redemption
+Enter 3rd favourite movie: The Dark Knight
+['Inception', 'The Shawshank Redemption', 'The Dark Knight']
+'''
+#practice question 
+''' WAP to check if a list contains a palindrome of elements.(Hint use copy())  '''
+list1 = [1,2,1]
+
+
+copy_l1 = list1.copy()
+copy_l1.reverse()
+
+if(copy_l1==list1):
+    print("Yes ",list1," is palindrome ")
+else:
+    print("No , list 1 is not a palindrome ")
+    
+list2=[1,2,3]
+copy_l2 = list2.copy()
+copy_l2.reverse()
+
+if(copy_l2==list1):
+    print("Yes ",list2 ,"is palindrome ")
+else:
+    print("No , ",list2," is not a palindrome ")
