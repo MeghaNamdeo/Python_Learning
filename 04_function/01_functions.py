b3b24032d8db2e73e9cd35cd98eab9643ce3b95d
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-21 10:41:42 +0000

    py

--- 04_function/01_functions.py
+++ 04_function/01_functions.py
@@ -1,3 +1,12 @@
-def print_hello():
-    print("hello")
-print()
+'''Functions in python 
+Block of statement that perform a specific task]
+
+def func_name(param1,param2):
+    #some work
+    return va
+func_name(agr1,arg2..)#function call
+'''
+
+def Sum(a,b):
+    Sum = a+b
+    return Sum
