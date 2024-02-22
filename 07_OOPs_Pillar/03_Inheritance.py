8de10541ec865f008d0b91bdeceeec35a1db6dd3
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-22 09:16:17 +0000

    py

---
+++ 07_OOPs_Pillar/03_Inheritance.py
@@ -0,0 +1,10 @@
+#inheritance 
+'''
+When one class(child / derived )derives thr properties and methods of another (parent/base )class.
+
+class Car:
+    ...
+class ToyotoCar(Car):
+    ....
+
+'''
